19026b8e8aae4137140f35135ae84a17c2ef319d
Amit Jain <amitjain2803@icloud.com>
2017-09-17 21:12:28 +0530

    Running Get API. Working on Post now

---
+++ app/models/student.js
@@ -0,0 +1,10 @@
+// student.js
+
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var StudentSchema   = new Schema({
+    name: String
+});
+
+module.exports = mongoose.model('Student', StudentSchema);
