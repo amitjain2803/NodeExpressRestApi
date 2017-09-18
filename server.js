014baa980212e3a21dc6426a26e5b1c198ec1de3
Amit Jain <amitjain2803@icloud.com>
2017-09-18 13:56:18 +0530

    Working RestFUL API. Need to add mongodb operations now.

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 // server.js
 
+
+/*
+/api/students	                GET	        Get all the students.
+/api/students	                POST	    Create a student.
+/api/students/:student_id	    GET	        Get a single student.
+/api/students/:student_id	    PUT	        Update a student with new info.
+/api/students/:student_id	    DELETE	    Delete a student.
+*/
+
+
+
 // BASE SETUP
 // =============================================================================
 
 // call the packages we need
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
-var bodyParser = require('body-parser');
-var mongoose   = require('mongoose');
-var Student     = require('./app/models/student');
+var bodyParser = require('body-parser');    // Body parser for JSON parsing
 
 
 // configure app to use bodyParser()
@@ -22,6 +31,7 @@ var port = process.env.PORT || 8080;        // set our port
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
+
 // middleware to use for all requests
 router.use(function(req, res, next) {
     // do logging
@@ -30,37 +40,48 @@ router.use(function(req, res, next) {
 });
 
 
+// API for all students.
+router.route('/students')
+
+       .post(function(req, res){
+            //console.log('coming to students ',req.body.name);
+           res.json({ message: 'Push to the database '+req.body.name });
+            // add to database
+       })
+        // get all the students (accessed at GET http://localhost:8080/api/bears)
+
+       .get(function(req, res) {
+          // console.log('get from the database ')
+           res.json({ message: 'get from the database ' });
+       });
 
 
-router.post('/students', function(req, res){
-    console.log('coming to students');
-    var student = new Student();
-    student.name = req.body.name;
-    student.save(function(err){
-        if(err)
-        {
-            res.send(err);
-        }
-        res.json({message:'student created'});
-    })
+// API specific to a student.
+router.route('/students/:student_id')
 
-});
+        .get(function(req,res){
+            res.json({message:'get the student with id = '+req.params.student_id})
+        })
+
+        .put(function(req,res){
+            res.json({message:'find and update student with id ='+req.params.student_id})
+        });
+
+        .delete(function(req,res){
+            res.json({message:'find and delete student with id ='+req.params.student_id})
+        });
+
+
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+    res.json({ message: 'welcome to my api!' });
 });
 
 
-
-// more routes for our API will happen here
-
-// REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/api', router);
 
-//connect to mongo db
-mongoose.connect('mongodb://amit:amit1234@localhost:27017/student'); // connect to local database
 
 // START THE SERVER
 // =============================================================================
