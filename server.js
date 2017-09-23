c9eeddb33b48135bb68d546e8adace5237f2a5e8
Amit Jain <amitjain2803@icloud.com>
2017-09-23 21:37:22 +0530

    Added Multer

--- server.js
+++ server.js
@@ -18,8 +18,12 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');    // Body parser for JSON parsing
+var multer = require('multer');
+var util = require("util");
+var fs = require("fs"); // this is to upload a file to the server
 
 
+app.use(multer({dest:'./uploads/'}).single('zip'));
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -32,12 +36,42 @@ var port = process.env.PORT || 8080;        // set our port
 var router = express.Router();              // get an instance of the express Router
 
 
+
 // middleware to use for all requests
 router.use(function(req, res, next) {
     // do logging
     console.log('Something is happening.');
     next(); // make sure we go to the next routes and don't stop here
 });
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+        console.log(file,"amit")
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+})
+
+var upload = multer({ storage: storage })
+
+// API for all students.
+router.route('/upload')
+
+    .post(function(req, res){
+        if (req.file) {
+           // console.log(util.inspect(req.file));
+            if (req.file.size === 0) return next(new Error("Hey, first would you select a file?"));
+            fs.exists(req.file.path, function(exists) {
+                if(exists) {
+                    res.end("Got your file!");
+                } else {
+                    res.end("Well, there is no magic for those who don’t believe in it!");
+                }
+            });
+        }
+    });
 
 
 // API for all students.
@@ -60,7 +94,7 @@ router.route('/students')
 router.route('/students/:student_id')
 
         .get(function(req,res){
-            res.json({message:'get the student with id = '+req.params.student_id};
+            res.json({message:'get the student with id = '+req.params.student_id});
         })
 
         .put(function(req,res){
